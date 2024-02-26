ad2fc105b57da61f95401331a9f25fce6781c6f7
GEN947 <leavegood1@gmail.com>
2024-02-26 15:11:26 +0900

    2024-02-26 first commit

---
+++ java/src/day1/Example03.java
@@ -0,0 +1,27 @@
+package day1;
+
+// if문
+public class Example03 {
+	public static void main(String[] args) {
+		int nai = 35;
+		if(nai >= 65) {
+			System.out.println("무료");
+		} else if(nai >= 25) {
+			System.out.println("대인(3000)");
+		} else {
+			// else는 조건을 적지 않는다(don't care)
+			// else 조건이 쉽게 보일 때 사용
+			System.out.println("무료");
+		}
+		int 고과=70; 매출액=80; 수금액=70;
+		if(고과>=70) {
+			
+		} else if (매출액>=10000) {
+			
+		} else if (수금액>=10000) {
+			
+		} else {
+			// 조건이 파악이 안 된다
+		}
+	}
+}
---
+++ java/src/day1/Example05.java
@@ -0,0 +1,25 @@
+package day1;
+
+import java.util.ArrayList;
+
+// for문: 일반 for, 향상된 for(js의 of, python의 in)
+public class Example05 {
+	public static void main(String[] args) {
+		ArrayList list1 = new ArrayList<>();
+		
+		list1.add(10);
+		list1.add(20);
+		list1.add(30);
+		
+		for(int i=0; i<3; i++) {
+			System.out.println(list1.get(i));
+		}
+		
+		// list1은 객체(Object) -> 객체는 자기일은 자기가 알아서 하는 S/W 부품
+		// list1의 크기는 자기가 알고 있어야 한다
+		
+		for(int i=0; i<list1.size(); i++) {
+			System.out.println(list1.get(i));
+		}
+	}
+}
